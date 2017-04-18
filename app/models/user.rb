e2fe94af9a2ab9fedf10902ec852fb453837865d
vincent jonathan <v.jonathan94@gmail.com>
2017-04-18 10:34:23 +0700

    Review model, migrations and model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :plays
+  has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
